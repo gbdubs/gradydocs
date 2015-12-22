acd1352cd3ade24736d7e8c1a3edfca5bb3f30a3
gradyberryward <gward@brandeis.edu>
2015-12-21 22:32:37 -0500

    Got the insertion/cursor business figured out!

--- js/src/io.js
+++ js/src/io.js
@@ -3,26 +3,29 @@
 
 $(document).ready(function(){
 
-
-
+	var cursor = 1;
 
 	function getChunkElement(n){
-		return $("#chunk-"+n);
+		return $("#chunk-"+n)[0];
 	}
 
+	function changeCursorPositionTo(newCursorElement){
+		$(getChunkElement(cursor)).removeClass("listening");
+		cursor = parseInt($(newCursorElement).attr('id').substring(6));
+		$(newCursorElement).addClass("listening");
+	}
+
+	function changeCursorPositionToId(id){
+		changeCursorPositionTo(getChunkElement(id));
+	}
+
+	
 
 	var firstChunk = createChunk(1);
 	firstChunk.content = "A";
-
-	$("#chunks").append(firstChunk.createElement());
-
-
-	var cursor = 1;
-
-	function changeCursor(){
-		cursor = parseInt($(this).attr('id').substring(6));
-		console.log(cursor);
-	}
+	var firstChunkElement = firstChunk.createElement()
+	$("#chunks").append(firstChunkElement);
+	changeCursorPositionToId(1);
 
 	var CHUNKID = 2;
 
@@ -33,9 +36,10 @@ $(document).ready(function(){
 		var cursorElement = getChunkElement(cursor);
 		var newElement = newChunk.createElement();
 		$(cursorElement).after(newElement);
-		$(newElement).click(changeCursor);
+		$(getChunkElement(newChunk.id)).click(function(){
+			changeCursorPositionTo(this);
+		});
+		changeCursorPositionToId(newChunk.id);
 		cursor = newChunk.id;
 	});
-
-	$(".chunk").click(changeCursor);
 });
