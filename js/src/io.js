2f743693fe31f0dccbc273dbe117cb3891b0da51
gradyberryward <gward@brandeis.edu>
2015-12-21 23:06:37 -0500

    Got some more features working.  Working on cleaning up the code to be somewhat clean

--- js/src/io.js
+++ js/src/io.js
@@ -1,15 +1,20 @@
 
 
-
 $(document).ready(function(){
 
+
+
+
 	var cursor = 1;
+	var cursorElement = getChunkElement(cursor);
 
 	function getChunkElement(n){
 		return $("#chunk-"+n)[0];
 	}
 
 	function changeCursorPositionTo(newCursorElement){
+
+
 		$(getChunkElement(cursor)).removeClass("listening");
 		cursor = parseInt($(newCursorElement).attr('id').substring(6));
 		$(newCursorElement).addClass("listening");
@@ -29,8 +34,8 @@ $(document).ready(function(){
 
 	var CHUNKID = 2;
 
-	$(window).keypress(function(e){
-		var newCharacter = String.fromCharCode(e.which);
+	function addCharacterToDocument(char){
+		var newCharacter = String.fromCharCode(char);
 		var newChunk = createChunk(CHUNKID++, cursor);
 		newChunk.content = newCharacter;
 		var cursorElement = getChunkElement(cursor);
@@ -41,5 +46,33 @@ $(document).ready(function(){
 		});
 		changeCursorPositionToId(newChunk.id);
 		cursor = newChunk.id;
+	}
+
+	function removeCharacterFromDocument(){
+		var previous = CHUNKS[cursor].previousChunk;
+		var element = getChunkElement(cursor);
+		deleteChunk(cursor);
+		element.remove();
+		changeCursorPositionToId(previous);
+	}
+
+	$(window).keydown(function(e){
+		if (e.which == 8) {
+			removeCharacterFromDocument();
+		} else if (e.which == 13){
+			addCharacterToDocument(13);
+			e.preventDefault();
+		} else if (e.which == 9){
+			addCharacterToDocument(9);
+			e.preventDefault();
+		} else if (e.which == 37){
+
+		}
+	});
+	$(window).keypress(function(e){
+		var c = e.which;
+		if (c >= 32 && c <= 126){
+			addCharacterToDocument(c);
+		}
 	});
 });
